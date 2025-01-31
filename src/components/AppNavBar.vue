0d36a450ace72f10aeb230094b5e82f572bf18fa
hfbmb <ondirisbibisara4@gmail.com>
2025-01-31 18:11:31 +0500

    cart icon with counter in nav bar

--- src/components/AppNavBar.vue
+++ src/components/AppNavBar.vue
@@ -20,10 +20,12 @@
 
     <v-btn
      	:to="'cart'"
-     	text="Cart"
-			slim
-			class="ms-3"
-    />
+     	variant="text"
+  		class="d-flex align-center ms-2"
+    >
+    	<v-icon color="black" size="24">mdi-cart</v-icon>
+			<span class="text-subtitle-1 ms-1 font-weight-medium">0</span>
+		</v-btn>
 
 		<v-btn
 			:to="'table'"
